75ba538a25618f45af290c4f145f05dacc2f0888
dxy <dxy493743828@qq.com>
2017-05-18 17:49:50 +0800

    Lambada表达式的用法

---
+++ app/src/main/java/com/android/lambdademo/MainActivity.java
@@ -0,0 +1,46 @@
+package com.android.lambdademo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = new Button(this);
+        //普通写法
+        new Thread(new Runnable() {
+            public void run() {
+                Log.d("输出:", "Hello World!");
+            }
+        }).start();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.postDelayed(new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d("延迟2秒", "输出按钮!");
+                    }
+                }),2000);
+            }
+        });
+
+        //Lambda表达式
+        new Thread(()->{
+            Log.d("输出:", "Hello World!");
+        });
+        button.setOnClickListener(view ->{
+            view.postDelayed(() ->{
+                Log.d("延迟2秒", "输出按钮!");
+            },2000);
+        });
+    }
+
+}
